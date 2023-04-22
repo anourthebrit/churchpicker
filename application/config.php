fa272a69dd64a7016b323f47eb841377dfca06ef
anourthebrit <63588928+anourthebrit@users.noreply.github.com>
2023-04-22 17:05:59 +0100

    removed logic file and included switch logic in index file

--- application/config.php
+++ application/config.php
@@ -1,5 +1,15 @@
 <?php
 // database connection code
-$con = mysqli_connect('localhost','root','','contact');
+$servername = "localhost";
+$username = "id20501286_anour";
+$password = "!Notravezzy67?";
+$database = "id20501286_churchpicker";
 
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
--- application/logic.php
+++
@@ -1,3 +0,0 @@
-<?php 
-//insert switch logic here
-?>
